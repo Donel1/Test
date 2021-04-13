8808a8702b0818237fa6de528a7c1bd00ee7bfc0
Donel1 <82468576+Donel1@users.noreply.github.com>
2021-04-13 20:48:06 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6075c8436de461001eb86de4",
+				"6075c8436de461001eb86de6"
+			]
+		},
+		"6075c8436de461001eb86de4": {
+			"id": "6075c8436de461001eb86de4",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6075c8436de461001eb86de6": {
+			"id": "6075c8436de461001eb86de6",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
