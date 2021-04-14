2de8b0b9f6b33f11a76a3a1d13bbab575b962045
Donel1 <82468576+Donel1@users.noreply.github.com>
2021-04-14 19:03:09 +0300

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Link, Image, Text, Box } from "@quarkly/widgets";
+import { Theme, Link, Text, Box } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { GlobalQuarklyPageStyles } from "global-page-styles";
+import { StackItem } from "@quarkly/components";
 export default (() => {
 	return <Theme theme={theme}>
 		<GlobalQuarklyPageStyles pageUrl={"index"} />
@@ -13,86 +14,122 @@ export default (() => {
 			<meta name={"description"} content={"Web site created using quarkly.io"} />
 			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
 		</Helmet>
-		<Image width="388.32px" height="388.32px" src="https://uploads.quarkly.io/6075c8436de461001eb86de1/images/Rectangle%2014%20(1).png?v=2021-04-13T16:48:38.504Z" margin="91.35px 0px 0px 148.74px" />
-		<Image height="388.32px" src="https://uploads.quarkly.io/6075c8436de461001eb86de1/images/Rectangle%2014%20(1).png?v=2021-04-13T16:48:38.504Z" width="388.32px" margin="0px 0px 0px 60px" />
-		<Box
-			width="383.32px"
-			height="241.15px"
-			margin="-15px 0px 0px 149.74px"
-			border-color="#EFEFEF"
-			border-width="1px"
-			border-style="solid"
-			border-radius="10px"
-		>
-			<Text font="700 24px --fontFamily-googleRoboto" margin="50px 0px 16px 50px" width="80px">
-				5 490р
-			</Text>
+		<StackItem height="1000px" color="#000000" background="#ffffff">
 			<Text
-				font="400 18px --fontFamily-googleRoboto"
-				width="55px"
-				margin="-42px 0px 16px 140px"
-				color="#8F8F8F"
-				text-decoration-line="line-through"
+				width=" 263px"
+				height=" 96px"
+				margin="63px 0px 16px 325px"
+				color="rgba(140, 140, 140, 0.58)"
+				font="300 16px --fontFamily-googleRoboto"
+				lg-margin="63px 0px 16px 60px"
+				md-margin="63px 0px 16px 50"
+				sm-margin="750px 0px 16px 15px"
 			>
-				6 190р
-			</Text>
-			<Text width="40px" font="500 18px --fontFamily-googleRoboto" margin="-37px 0px 16px 223px" color="#EC7623">
-				-10%
+				2004-2021 © Wildberries — модный интернет-магазин одежды, обуви{" "}
+				<br />
+				и аксессуаров. Все права защищены.
 			</Text>
 			<Box
-				width="65.29px"
-				height="34.29px"
-				color="#FDF1E9"
-				background="#FDF1E9"
-				border-radius="5px"
-				margin="-43px 0px 0px 211px"
-			/>
-			<Text width="300px" margin="25px 0px 16px 50px" font="350 16px/150% sans-serif">
-				Двухъярусный торт с подтеками шоколада и свежими ягодами
-			</Text>
-			<Text width="300px" margin="25px 0px 16px 50px" font="350 16px/150% sans-serif" color="#36CB84">
-				Доставка завтра
-			</Text>
-		</Box>
-		<Box
-			width="383.32px"
-			height="241.15px"
-			margin="-240px 0px 0px 598px"
-			border-color="#EFEFEF"
-			border-width="1px"
-			border-style="solid"
-			border-radius="10px"
-		>
-			<Text font="700 24px --fontFamily-googleRoboto" margin="50px 0px 16px 50px" width="80px">
-				5 490р
-			</Text>
-			<Text
-				font="400 18px --fontFamily-googleRoboto"
-				width="55px"
-				margin="-42px 0px 16px 140px"
-				color="#8F8F8F"
-				text-decoration-line="line-through"
+				width="210px"
+				margin="63px 0px 0px 69px"
+				height="350px"
+				md-margin="63px 0px 0px 70px"
+				sm-margin="63px 0px 0px -260px"
 			>
-				6 190р
-			</Text>
-			<Text width="40px" font="500 18px --fontFamily-googleRoboto" margin="-37px 0px 16px 223px" color="#EC7623">
-				-10%
-			</Text>
+				<Text height="18px" color="#000000" font="400 16px --fontFamily-googleRoboto" margin="0 0px 16px 0px">
+					Покупателям
+				</Text>
+				<Text
+					color="rgba(0, 0, 0, 0.58)"
+					font="300 16px/120% --fontFamily-googleRoboto"
+					width="204.95px"
+					height="300px"
+					margin="20px 0px 16px 0px"
+				>
+					Как сделать заказ{"\n"}
+					<br />
+					<br />
+					Способы оплаты{"\n"}
+					<br />
+					<br />
+					Доставка{"\n"}
+					<br />
+					<br />
+					Возврат товара{"\n"}
+					<br />
+					<br />
+					Возврат денежных средств{"\n"}
+					<br />
+					<br />
+					Правила продажи{"\n"}
+					<br />
+					<br />
+					Правила пользования торговой площадкой{"\n"}
+					<br />
+					<br />
+					Вопросы и ответы
+				</Text>
+			</Box>
 			<Box
-				width="65.29px"
-				height="34.29px"
-				color="#FDF1E9"
-				background="#FDF1E9"
-				border-radius="5px"
-				margin="-43px 0px 0px 211px"
-			/>
-			<Text width="300px" margin="25px 0px 16px 50px" font="350 16px/150% sans-serif">
-				Двухъярусный торт с подтеками шоколада и свежими ягодами
-			</Text>
-			<Text width="300px" margin="25px 0px 16px 50px" font="350 16px/150% sans-serif" color="#36CB84">
-				Доставка завтра
-			</Text>
-		</Box>
+				width="210px"
+				margin="63px 0px 0px 69px"
+				height="350px"
+				md-margin="500px 0px 0px -212px"
+				sm-margin="63px 0px 0px 20px"
+			>
+				<Text color="#000000" font="400 16px --fontFamily-googleRoboto" height="18px" margin="0 0px 16px 0px">
+					Партнерам
+				</Text>
+				<Text
+					color="rgba(0, 0, 0, 0.58)"
+					font="300 16px/120% --fontFamily-googleRoboto"
+					width="204.95px"
+					height="300px"
+					margin="20px 0px 16px 0px"
+				>
+					Партнерская программа{"\n"}
+					<br />
+					<br />
+					Как начать сотрудничество{"\n"}
+					<br />
+					<br />
+					Преимущества{"\n"}
+					<br />
+					<br />
+					Франшиза{"\n"}
+				</Text>
+			</Box>
+			<Box
+				width="210px"
+				margin="63px 325px 0px 69px"
+				height="350px"
+				lg-margin="500px 0px 0px -820px"
+				md-margin="500px 0px 0px -540px"
+				sm-margin="500px 0px 0px -440px"
+			>
+				<Text color="#000000" font="400 16px --fontFamily-googleRoboto" height="18px" margin="0 0px 16px 0px">
+					Компания
+				</Text>
+				<Text
+					color="rgba(0, 0, 0, 0.58)"
+					font="300 16px/120% --fontFamily-googleRoboto"
+					width="204.95px"
+					height="300px"
+					margin="20px 0px 16px 0px"
+				>
+					О нас{"\n"}
+					<br />
+					<br />
+					Реквизиты{"\n"}
+					<br />
+					<br />
+					Пресс-центр{"\n"}
+					<br />
+					<br />
+					Контакты{"\n"}
+				</Text>
+			</Box>
+		</StackItem>
 		<Link
 			font={"--capture"}
 			font-size={"10px"}
