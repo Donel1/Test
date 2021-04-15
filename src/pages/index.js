00ab56ac76bce8dc2a2e81014c2a34576ed2fa74
Donel1 <82468576+Donel1@users.noreply.github.com>
2021-04-15 12:35:34 +0300

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Link, Box, Text, Image } from "@quarkly/widgets";
+import { Theme, Link, Text, Box, Button, Image } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { GlobalQuarklyPageStyles } from "global-page-styles";
 import { Override, StackItem, Stack, Section } from "@quarkly/components";
@@ -19,66 +19,20 @@ export default (() => {
 			<Stack margin-top="40px">
 				<StackItem width="25%" lg-width="50%" sm-width="100%">
 					<Override slot="StackItemContent" flex-direction="column" />
-					<Box
-						height="0"
-						margin="0 0 20px 0"
-						padding-bottom="100%"
-						background="url(https://images.unsplash.com/photo-1618329254028-833d820b7f86?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=2000) 50% 0/cover no-repeat"
-						border-radius="10px"
-					/>
-					<Stack>
-						{"    "}
-						<StackItem width="100%" display="flex" padding="16px 16px 0 16px">
-							<Override slot="StackItemContent" />
-							{"        "}
-							<Text as="h3" font="normal 500 24px/1.2 --fontFamily-googleRoboto" margin="2px 15px 16px 0px">
-								5 490р
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://uploads.quarkly.io/6075c8436de461001eb86de1/images/Group%20224.png?v=2021-04-15T08:34:06.729Z) 50% 0/cover no-repeat">
+						<Box background="#EBFAF3" width="134.86px" height="43.55px">
+							<Text margin="0 0px 16px 22px" border-color="#36CB84" color="#36CB84" font="400 16px/45px --fontFamily-googleRoboto">
+								Хит продаж
 							</Text>
-							<Text
-								font="normal 300 16px/2.1 --fontFamily-googleRoboto"
-								margin="0px 15px 0px 0px"
-								display="inline-block"
-								color="#8F8F8F"
-								text-decoration-line="line-through"
-								height="50% content-box"
-							>
-								6 190р
-							</Text>
-							<Box background="#FDF1E9" width="65px" height="34px" border-radius="5px">
-								<Text margin="6px 0px 16px 13px" font="500 16px/24px --fontFamily-googleRoboto" color="#EC7623">
-									-10%
-								</Text>
-							</Box>
-							{"    "}
-						</StackItem>
-						{"    "}
-						<StackItem width="30%" display="flex">
-							{"        "}{"    "}
-						</StackItem>
-					</Stack>
-					<Text as="p" margin="-30px 0px 16px 0px" color="#000000" font="300 14pt/24px --fontFamily-googleRoboto">
-						Nike / Кроссовки NIKE REVOLUTION 5 FLYEASE
-					</Text>
-					<Text as="p" margin="2px 0px 16px 0px" color="#36CB84" font="300 14pt/24px --fontFamily-googleRoboto">
-						Доставка завтра
-					</Text>
-				</StackItem>
-				<StackItem width="25%" lg-width="50%" sm-width="100%">
-					<Override slot="StackItemContent" flex-direction="column" />
-					<Box
-						height="0"
-						margin="0 0 20px 0"
-						padding-bottom="100%"
-						background="url(https://images.unsplash.com/photo-1618329254028-833d820b7f86?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=2000) 50% 0/cover no-repeat"
-						border-radius="10px"
-					/>
+						</Box>
+					</Box>
 					<Stack>
 						{"    "}
 						<StackItem display="flex" width="100%" padding="16px 16px 0 16px">
 							<Override slot="StackItemContent" />
 							{"        "}
 							<Text as="h3" font="normal 500 24px/1.2 --fontFamily-googleRoboto" margin="2px 15px 16px 0px">
-								5 490р
+								3 134р
 							</Text>
 							<Text
 								font="normal 300 16px/2.1 --fontFamily-googleRoboto"
@@ -88,10 +42,10 @@ export default (() => {
 								text-decoration-line="line-through"
 								height="50% content-box"
 							>
-								6 190р
+								5 490р
 							</Text>
 							<Box background="#FDF1E9" width="65px" height="34px" border-radius="5px">
-								<Text margin="6px 0px 16px 13px" font="500 16px/24px --fontFamily-googleRoboto" color="#EC7623">
+								<Text margin="6px 0px 16px 14.5px" font="500 16px/24px --fontFamily-googleRoboto" color="#EC7623">
 									-10%
 								</Text>
 							</Box>
@@ -103,28 +57,35 @@ export default (() => {
 						</StackItem>
 					</Stack>
 					<Text as="p" margin="-30px 0px 16px 0px" font="300 14pt/24px --fontFamily-googleRoboto" color="#000000">
-						Nike / Кроссовки NIKE REVOLUTION 5 FLYEASE
+						Nike / Кроссовки TANJUN
 					</Text>
 					<Text as="p" margin="2px 0px 16px 0px" font="300 14pt/24px --fontFamily-googleRoboto" color="#36CB84">
 						Доставка завтра
 					</Text>
+					<Button
+						background="#ffffff"
+						color="#b9b9b9"
+						border-width="1px"
+						border-color="#e6e6e6"
+						border-style="solid"
+						height="59px"
+						font="normal 300 18px/1.5 --fontFamily-googleRoboto"
+						margin="15px 0px 0px 0px"
+						border-radius="7px"
+					>
+						В корзину
+					</Button>
 				</StackItem>
 				<StackItem width="25%" lg-width="50%" sm-width="100%">
 					<Override slot="StackItemContent" flex-direction="column" />
-					<Box
-						height="0"
-						margin="0 0 20px 0"
-						padding-bottom="100%"
-						background="url(https://images.unsplash.com/photo-1618329254028-833d820b7f86?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=2000) 50% 0/cover no-repeat"
-						border-radius="10px"
-					/>
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://uploads.quarkly.io/6075c8436de461001eb86de1/images/Group%20224.png?v=2021-04-15T08:34:06.729Z) 50% 0/cover no-repeat" />
 					<Stack>
 						{"    "}
 						<StackItem display="flex" width="100%" padding="16px 16px 0 16px">
 							<Override slot="StackItemContent" />
 							{"        "}
 							<Text as="h3" font="normal 500 24px/1.2 --fontFamily-googleRoboto" margin="2px 15px 16px 0px">
-								5 490р
+								3 134р
 							</Text>
 							<Text
 								font="normal 300 16px/2.1 --fontFamily-googleRoboto"
@@ -134,10 +95,10 @@ export default (() => {
 								text-decoration-line="line-through"
 								height="50% content-box"
 							>
-								6 190р
+								5 490р
 							</Text>
 							<Box background="#FDF1E9" width="65px" height="34px" border-radius="5px">
-								<Text margin="6px 0px 16px 13px" font="500 16px/24px --fontFamily-googleRoboto" color="#EC7623">
+								<Text margin="6px 0px 16px 14.5px" font="500 16px/24px --fontFamily-googleRoboto" color="#EC7623">
 									-10%
 								</Text>
 							</Box>
@@ -149,28 +110,35 @@ export default (() => {
 						</StackItem>
 					</Stack>
 					<Text as="p" margin="-30px 0px 16px 0px" font="300 14pt/24px --fontFamily-googleRoboto" color="#000000">
-						Nike / Кроссовки NIKE REVOLUTION 5 FLYEASE
+						Nike / Кроссовки TANJUN
 					</Text>
 					<Text as="p" margin="2px 0px 16px 0px" font="300 14pt/24px --fontFamily-googleRoboto" color="#36CB84">
 						Доставка завтра
 					</Text>
+					<Button
+						background="#ffffff"
+						color="#b9b9b9"
+						border-width="1px"
+						border-color="#e6e6e6"
+						border-style="solid"
+						height="59px"
+						font="normal 300 18px/1.5 --fontFamily-googleRoboto"
+						margin="15px 0px 0px 0px"
+						border-radius="7px"
+					>
+						В корзину
+					</Button>
 				</StackItem>
 				<StackItem width="25%" lg-width="50%" sm-width="100%">
 					<Override slot="StackItemContent" flex-direction="column" />
-					<Box
-						height="0"
-						margin="0 0 20px 0"
-						padding-bottom="100%"
-						background="url(https://images.unsplash.com/photo-1618329254028-833d820b7f86?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=2000) 50% 0/cover no-repeat"
-						border-radius="10px"
-					/>
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://uploads.quarkly.io/6075c8436de461001eb86de1/images/Group%20224.png?v=2021-04-15T08:34:06.729Z) 50% 0/cover no-repeat" />
 					<Stack>
 						{"    "}
 						<StackItem display="flex" width="100%" padding="16px 16px 0 16px">
 							<Override slot="StackItemContent" />
 							{"        "}
 							<Text as="h3" font="normal 500 24px/1.2 --fontFamily-googleRoboto" margin="2px 15px 16px 0px">
-								5 490р
+								3 134р
 							</Text>
 							<Text
 								font="normal 300 16px/2.1 --fontFamily-googleRoboto"
@@ -180,10 +148,10 @@ export default (() => {
 								text-decoration-line="line-through"
 								height="50% content-box"
 							>
-								6 190р
+								5 490р
 							</Text>
 							<Box background="#FDF1E9" width="65px" height="34px" border-radius="5px">
-								<Text margin="6px 0px 16px 13px" font="500 16px/24px --fontFamily-googleRoboto" color="#EC7623">
+								<Text margin="6px 0px 16px 14.5px" font="500 16px/24px --fontFamily-googleRoboto" color="#EC7623">
 									-10%
 								</Text>
 							</Box>
@@ -195,16 +163,82 @@ export default (() => {
 						</StackItem>
 					</Stack>
 					<Text as="p" margin="-30px 0px 16px 0px" font="300 14pt/24px --fontFamily-googleRoboto" color="#000000">
-						Nike / Кроссовки NIKE REVOLUTION 5 FLYEASE
+						Nike / Кроссовки TANJUN
 					</Text>
 					<Text as="p" margin="2px 0px 16px 0px" font="300 14pt/24px --fontFamily-googleRoboto" color="#36CB84">
 						Доставка завтра
 					</Text>
+					<Button
+						background="#ffffff"
+						color="#b9b9b9"
+						border-width="1px"
+						border-color="#e6e6e6"
+						border-style="solid"
+						height="59px"
+						font="normal 300 18px/1.5 --fontFamily-googleRoboto"
+						margin="15px 0px 0px 0px"
+						border-radius="7px"
+					>
+						В корзину
+					</Button>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://uploads.quarkly.io/6075c8436de461001eb86de1/images/Group%20224.png?v=2021-04-15T08:34:06.729Z) 50% 0/cover no-repeat" />
+					<Stack>
+						{"    "}
+						<StackItem display="flex" width="100%" padding="16px 16px 0 16px">
+							<Override slot="StackItemContent" />
+							{"        "}
+							<Text as="h3" font="normal 500 24px/1.2 --fontFamily-googleRoboto" margin="2px 15px 16px 0px">
+								3 134р
+							</Text>
+							<Text
+								font="normal 300 16px/2.1 --fontFamily-googleRoboto"
+								margin="0px 15px 0px 0px"
+								display="inline-block"
+								color="#8F8F8F"
+								text-decoration-line="line-through"
+								height="50% content-box"
+							>
+								5 490р
+							</Text>
+							<Box background="#FDF1E9" width="65px" height="34px" border-radius="5px">
+								<Text margin="6px 0px 16px 14.5px" font="500 16px/24px --fontFamily-googleRoboto" color="#EC7623">
+									-10%
+								</Text>
+							</Box>
+							{"    "}
+						</StackItem>
+						{"    "}
+						<StackItem width="30%" display="flex">
+							{"        "}{"    "}
+						</StackItem>
+					</Stack>
+					<Text as="p" margin="-30px 0px 16px 0px" font="300 14pt/24px --fontFamily-googleRoboto" color="#000000">
+						Nike / Кроссовки TANJUN
+					</Text>
+					<Text as="p" margin="2px 0px 16px 0px" font="300 14pt/24px --fontFamily-googleRoboto" color="#36CB84">
+						Доставка завтра
+					</Text>
+					<Button
+						background="#ffffff"
+						color="#b9b9b9"
+						border-width="1px"
+						border-color="#e6e6e6"
+						border-style="solid"
+						height="59px"
+						font="normal 300 18px/1.5 --fontFamily-googleRoboto"
+						margin="15px 0px 0px 0px"
+						border-radius="7px"
+					>
+						В корзину
+					</Button>
 				</StackItem>
 			</Stack>
 		</Section>
 		<Section padding="0 0 10px 0" sm-padding="40px 0" font="--base" color="--dark">
-			<Override slot="SectionContent" />
+			<Override slot="SectionContent" sm-margin="-105px 21.25px 0px 21.25px" />
 			<Stack margin-top="40px">
 				<StackItem width="25%" lg-width="50%" sm-width="100%">
 					<Override slot="StackItemContent" flex-direction="column" />
@@ -251,6 +285,19 @@ export default (() => {
 					<Text as="p" margin="2px 0px 16px 0px" font="300 14pt/24px --fontFamily-googleRoboto" color="#36CB84">
 						Доставка завтра
 					</Text>
+					<Button
+						background="#ffffff"
+						color="#b9b9b9"
+						border-width="1px"
+						border-color="#e6e6e6"
+						border-style="solid"
+						height="59px"
+						font="normal 300 18px/1.5 --fontFamily-googleRoboto"
+						margin="15px 0px 0px 0px"
+						border-radius="7px"
+					>
+						В корзину
+					</Button>
 				</StackItem>
 				<StackItem width="25%" lg-width="50%" sm-width="100%">
 					<Override slot="StackItemContent" flex-direction="column" />
@@ -297,6 +344,19 @@ export default (() => {
 					<Text as="p" margin="2px 0px 16px 0px" font="300 14pt/24px --fontFamily-googleRoboto" color="#36CB84">
 						Доставка завтра
 					</Text>
+					<Button
+						background="#ffffff"
+						color="#b9b9b9"
+						border-width="1px"
+						border-color="#e6e6e6"
+						border-style="solid"
+						height="59px"
+						font="normal 300 18px/1.5 --fontFamily-googleRoboto"
+						margin="15px 0px 0px 0px"
+						border-radius="7px"
+					>
+						В корзину
+					</Button>
 				</StackItem>
 				<StackItem width="25%" lg-width="50%" sm-width="100%">
 					<Override slot="StackItemContent" flex-direction="column" />
@@ -343,6 +403,19 @@ export default (() => {
 					<Text as="p" margin="2px 0px 16px 0px" font="300 14pt/24px --fontFamily-googleRoboto" color="#36CB84">
 						Доставка завтра
 					</Text>
+					<Button
+						background="#ffffff"
+						color="#b9b9b9"
+						border-width="1px"
+						border-color="#e6e6e6"
+						border-style="solid"
+						height="59px"
+						font="normal 300 18px/1.5 --fontFamily-googleRoboto"
+						margin="15px 0px 0px 0px"
+						border-radius="7px"
+					>
+						В корзину
+					</Button>
 				</StackItem>
 				<StackItem width="25%" lg-width="50%" sm-width="100%">
 					<Override slot="StackItemContent" flex-direction="column" />
@@ -389,18 +462,33 @@ export default (() => {
 					<Text as="p" margin="2px 0px 16px 0px" font="300 14pt/24px --fontFamily-googleRoboto" color="#36CB84">
 						Доставка завтра
 					</Text>
+					<Button
+						background="#ffffff"
+						color="#b9b9b9"
+						border-width="1px"
+						border-color="#e6e6e6"
+						border-style="solid"
+						height="59px"
+						font="normal 300 18px/1.5 --fontFamily-googleRoboto"
+						margin="15px 0px 0px 0px"
+						border-radius="7px"
+					>
+						В корзину
+					</Button>
 				</StackItem>
 			</Stack>
 		</Section>
 		<Section
-			padding="64px 0"
 			sm-padding="40px 0"
 			font="--base"
 			color="--dark"
 			width="100%"
+			lg-padding="64px 0 64px 55px"
+			md-padding="64px 0 64px 25px"
+			padding="80px 0 24px 0"
 		>
 			<Override slot="SectionContent" width="1280px" />
-			<Stack margin-top="40px" width="100%" margin="0 0 0 0">
+			<Stack width="100%" margin="0 0 0 0">
 				<StackItem
 					width="25%"
 					lg-width="30%"
@@ -409,6 +497,8 @@ export default (() => {
 					md-padding="16px 16px 16px 16px"
 					sm-height="415px"
 					sm-padding="40px 16px 16px 16px"
+					padding="0px 0px 0px 0px"
+					margin="0px px 0px -20px"
 				>
 					<Override slot="StackItemContent" flex-direction="column" lg-padding="16px 0px 0px 0px" md-padding="0 0px 0px 0px" />
 					<Text
@@ -418,12 +508,18 @@ export default (() => {
 						lg-margin="0 0px 16px 0"
 						sm-margin="0 0px 16px 0"
 					>
-						2004-2021 © Wildberries — модный интернет-магазин одежды, обуви{" "}
+						2004-2021 © Shop — модный интернет-магазин одежды, обуви{" "}
 						<br />
 						и аксессуаров. Все права защищены.
 					</Text>
 				</StackItem>
-				<StackItem width="15%" lg-width="30%" sm-width="50%" sm-margin-top="24px">
+				<StackItem
+					width="15%"
+					lg-width="30%"
+					sm-width="50%"
+					sm-margin-top="24px"
+					margin="0px 15px 0px 0px"
+				>
 					<Override slot="StackItemContent" flex-direction="column" />
 					<Text height="18px" color="#000000" font="400 16px --fontFamily-googleRoboto" margin="0 0px 16px 0px">
 						Покупателям
@@ -477,7 +573,13 @@ export default (() => {
 						Франшиза{"\n"}
 					</Text>
 				</StackItem>
-				<StackItem width="15%" lg-width="30%" sm-width="50%" sm-margin-top="24px">
+				<StackItem
+					width="15%"
+					lg-width="30%"
+					sm-width="50%"
+					sm-margin-top="24px"
+					margin="0px 15px 0px 0px"
+				>
 					<Override slot="StackItemContent" flex-direction="column" />
 					<Text color="#000000" font="400 16px --fontFamily-googleRoboto" height="18px" margin="0 0px 16px 0px">
 						Компания
@@ -495,7 +597,13 @@ export default (() => {
 						Контакты{"\n"}
 					</Text>
 				</StackItem>
-				<StackItem width="15%" lg-width="30%" sm-width="50%" sm-margin-top="24px">
+				<StackItem
+					width="15%"
+					lg-width="30%"
+					sm-width="50%"
+					sm-margin-top="24px"
+					margin="0px 15px 0px 0px"
+				>
 					<Override slot="StackItemContent" flex-direction="column" />
 					<Text color="#000000" font="400 16px --fontFamily-googleRoboto" height="18px" margin="0 0px 16px 0px">
 						Другое
@@ -508,9 +616,6 @@ export default (() => {
 						<br />
 						<br />
 						Online журнал
-						<br />
-						<br />
-						Wb.team
 						<br />
 						<br />
 						Перевозчикам
